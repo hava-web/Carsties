23e58e87ef16b1ac2f1bab06b9f1520aa33ca595
hava-web <nguyenngocha18062002@gmail.com>
2024-11-02 15:15:33 +0700

    add ignore

--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -8,9 +8,9 @@ public class UpdateAuctionDto
 
     public string Model { get; set; }
 
-    public int Year { get; set; }
+    public int? Year { get; set; }
 
     public string Color { get; set; }
 
-    public int Mileage { get; set; }
+    public int? Mileage { get; set; }
 }
