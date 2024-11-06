72ead4d02feb7eab5af6101be819186e45bb3f0d
hann1 <nguyenngocha18062002@gmail.com>
2024-11-06 14:21:16 +0700

    Delete obj folder

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+
+}
