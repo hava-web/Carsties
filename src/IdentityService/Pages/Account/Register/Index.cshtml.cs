03cc7bde16fef65470bdea3314581e29980ae4d9
hann1 <nguyenngocha18062002@gmail.com>
2024-12-06 16:38:29 +0700

    add register

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Account.Register
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
