19d52ee17b9a0cd3a11d37c6c092df613b523f2b
hann1 <nguyenngocha18062002@gmail.com>
2024-12-31 15:36:30 +0700

    add front end

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react';
+import { AiOutlineCar } from 'react-icons/ai';
+
+const Navbar = () => {
+    return (
+        <>
+            <header className='stricky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
+                <div >
+                    <AiOutlineCar size={34} />
+                    <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
+                        Carsties Auction
+                    </div>
+                </div>
+                <div className="">Search</div>
+                <div className="">Login</div>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  console.log("Server Component");
+
+  return (
+    <div>
+      Contents go here
+    </div>
+  );
+}
