04f2197aa235c4a1db600b5409b1e2b396622c10
hava-web <nguyenngocha18062002@gmail.com>
2025-01-06 06:03:42 +0700

    add zustand

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useEffect, useState } from 'react';
 import AuctionCard from './AuctionCard';
 import { Auction, PageResult } from '../types';
@@ -7,48 +7,50 @@ import { getData } from '../actions/auctionAction';
 import Filters from './Filters';
 
 export type FilterCommon = {
-    pageCount: number;
-    pageNumber: number;
-    pageSize: number;
-}
+  pageCount: number;
+  pageNumber: number;
+  pageSize: number;
+  searchTerm: string;
+};
 
 const Listings = () => {
-    const [auctions, setAuctions] = useState<Auction[]>([]);
-    const [filter, setFilter] = useState<FilterCommon>(
-        {
-            pageCount: 0,
-            pageNumber: 1,
-            pageSize: 4
-        }
-    );
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  const [filter, setFilter] = useState<FilterCommon>({
+    pageCount: 0,
+    pageNumber: 1,
+    pageSize: 4,
+    searchTerm: '',
+  });
 
-    useEffect(() => {
-        getData(filter.pageNumber, filter.pageSize).then((data: PageResult<Auction>) => {
-            setAuctions(data.results);
-            setFilter({
-                ...filter,
-                pageCount: data.pageCount
-            });
-        }).catch((error) => {
-            console.error('Error fetching data:', error);
+  useEffect(() => {
+    getData(filter.pageNumber, filter.pageSize)
+      .then((data: PageResult<Auction>) => {
+        setAuctions(data.results);
+        setFilter({
+          ...filter,
+          pageCount: data.pageCount,
         });
-    }, [filter.pageNumber, filter.pageSize]);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [filter.pageNumber, filter.pageSize]);
 
-    if (auctions.length === 0) return <div>Loading...</div>;
+  if (auctions.length === 0) return <div>Loading...</div>;
 
-    return (
-        <>
-            <Filters filter={filter} FilterChanged={setFilter} />
-            <div className='grid grid-cols-4 gap-6'>
-                {auctions.map((auction) => (
-                    <AuctionCard key={auction.id} auction={auction} />
-                ))}
-            </div>
-            <div className="flex justify-center mt-4">
-                <AppPagination pageChanged={setFilter} filter={filter} />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Filters filter={filter} FilterChanged={setFilter} />
+      <div className='grid grid-cols-4 gap-6'>
+        {auctions.map((auction) => (
+          <AuctionCard key={auction.id} auction={auction} />
+        ))}
+      </div>
+      <div className='flex justify-center mt-4'>
+        <AppPagination pageChanged={setFilter} filter={filter} />
+      </div>
+    </>
+  );
+};
 
-export default Listings
+export default Listings;
