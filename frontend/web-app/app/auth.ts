bc0e6deebd46178f388321bcbaffb7c79a3849a6
hann1 <nguyenngocha18062002@gmail.com>
2025-01-14 16:30:25 +0700

    add google authen

--- frontend/web-app/app/auth.ts
+++ frontend/web-app/app/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth, { Profile } from 'next-auth';
 import { OIDCConfig } from 'next-auth/providers';
 import DuendeIDS6Provider from 'next-auth/providers/duende-identity-server6';
+import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
@@ -15,5 +16,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       authorization: { params: { scope: 'openid profile auctionApp' } },
       idToken: true,
     } as OIDCConfig<Profile>),
+    Google({
+      clientId: '',
+      clientSecret: '',
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    })
   ],
 });
