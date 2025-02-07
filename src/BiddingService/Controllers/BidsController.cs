6f487c3fdb7a74f93148f1e0e379e929701e774d
hann1 <nguyenngocha18062002@gmail.com>
2025-02-07 16:11:15 +0700

    Add Grpc client,dockerize bidding service and modify gateway service

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using BiddingService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authorization;
@@ -15,11 +16,13 @@ namespace BiddingService.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly GrpcAuctionClient _grpcClient;
 
-        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
+        public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint, GrpcAuctionClient grpcClient)
         {
             _mapper = mapper;
             _publishEndpoint = publishEndpoint;
+            _grpcClient = grpcClient;
         }
 
 
@@ -31,7 +34,12 @@ namespace BiddingService.Controllers
 
             if (auction == null)
             {
-                return NotFound();
+                auction = _grpcClient.GetAuction(auctionId);
+
+                if (auction == null)
+                {
+                    return BadRequest("Cannot accept bids on this auction at this time");
+                }
             }
 
             if (auction.Seller == User.Identity.Name)
